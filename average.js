318567f3a73f7e274d443d78fd0953fb405cbaa9
kritikpancholi <47328383+kritikpancholi@users.noreply.github.com>
2021-12-04 16:43:22 +0530

    Update average.js

--- average.js
+++ average.js
@@ -1,5 +1,13 @@
 function average(numbers) {
-  return parseFloat(numbers.reduce((p, c)=> p + c, 0)) / numbers.length;
+  let count = 0;
+  return parseFloat(numbers.reduce((p, c)=> {
+    if (isNaN(c)){
+      count++;
+      return p + 0;  
+    } else {
+      return p + c;
+    }
+  }, 0)) / (numbers.length - count);
 }
 
 module.exports = {average};

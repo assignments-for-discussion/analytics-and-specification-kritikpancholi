d87e169ad446a363bc79612dff9593d95e6e773d
kritikpancholi <47328383+kritikpancholi@users.noreply.github.com>
2021-12-08 17:15:13 +0530

    Update average.test.js

--- test/average.test.js
+++ test/average.test.js
@@ -19,3 +19,7 @@ it('ignores NaN in the input', ()=> {
 it('round up reading upto 2 decimal places ', ()=> {
   expect(average( [10.89901,0.92,20.00001])).to.be.approximately(10.60,0.01);
 });
+
+it('computing average of list ignores readings greater than 100 and less than -100', ()=> {
+  expect(average( [50,120,-102,20])).to.be.approximately(35,0.01);
+});
